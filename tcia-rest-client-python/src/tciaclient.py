8c61ea94cea649ed5559f1828fa227dae87087fe
Ameen Kazerouni <ameen.kazerouni@emory.edu>
2014-10-24 16:30:23 -0400

    Code Cleanup. Added /query

--- tcia-rest-client-python/src/tciaclient.py
+++ tcia-rest-client-python/src/tciaclient.py
@@ -29,62 +29,62 @@ class TCIAClient:
         return resp
     
     def get_modality_values(self,collection = None , bodyPartExamined = None , modality = None , outputFormat = "json" ):
-        serviceUrl = self.baseUrl + "/" + self.GET_MODALITY_VALUES
+        serviceUrl = self.baseUrl + "/query/" + self.GET_MODALITY_VALUES
         queryParameters = {"Collection" : collection , "BodyPartExamined" : bodyPartExamined , "Modality" : modality , "format" : outputFormat }
         resp = self.execute(serviceUrl , queryParameters)
         return resp
     
     def get_series__size(self, SeriesInstanceUID = None, outputFormat = "json" ):
-        serviceUrl = self.baseUrl + "/" + self.GET_SERIES_SIZE
+        serviceUrl = self.baseUrl + "/query/" + self.GET_SERIES_SIZE
         queryParameters = {"SeriesInstanceUID" : SeriesInstanceUID, "format" :
                            outputFormat}
         resp = self.execute(serviceUrl, queryParameters)
         return resp
 
     def contents_by_name(self, name = None):
-        serviceUrl = self.baseUrl + "/" + self.CONTENTS_BY_NAME
+        serviceUrl = self.baseUrl + "/query/" + self.CONTENTS_BY_NAME
         queryParameters = {"name" : name}
         rep = self.execute(serviceUrl,queryParameters)
         return resp
 
     def get_manufacturer_values(self,collection = None , bodyPartExamined = None , modality = None , outputFormat = "json" ):
-        serviceUrl = self.baseUrl + "/" + self.GET_MANUFACTURER_VALUES
+        serviceUrl = self.baseUrl + "/query/" + self.GET_MANUFACTURER_VALUES
         queryParameters = {"Collection" : collection , "BodyPartExamined" : bodyPartExamined , "Modality" : modality , "format" : outputFormat }
         resp = self.execute(serviceUrl , queryParameters)
         return resp
         
     def get_collection_values(self,outputFormat = "json" ):
-        serviceUrl = self.baseUrl + "/" + self.GET_COLLECTION_VALUES
+        serviceUrl = self.baseUrl + "/query/" + self.GET_COLLECTION_VALUES
         queryParameters = { "format" : outputFormat }
         resp = self.execute(serviceUrl , queryParameters)
         return resp
         
     def get_body_part_values(self,collection = None , bodyPartExamined = None , modality = None , outputFormat = "json" ):
-        serviceUrl = self.baseUrl + "/" + self.GET_BODY_PART_VALUES
+        serviceUrl = self.baseUrl + "/query/" + self.GET_BODY_PART_VALUES
         queryParameters = {"Collection" : collection , "BodyPartExamined" : bodyPartExamined , "Modality" : modality , "format" : outputFormat }
         resp = self.execute(serviceUrl , queryParameters)
         return resp
     
     def get_patient_study(self,collection = None , patientId = None , studyInstanceUid = None , outputFormat = "json" ):
-        serviceUrl = self.baseUrl + "/" + self.GET_PATIENT_STUDY
+        serviceUrl = self.baseUrl + "/query/" + self.GET_PATIENT_STUDY
         queryParameters = {"Collection" : collection , "PatientID" : patientId , "StudyInstanceUID" : studyInstanceUid , "format" : outputFormat }
         resp = self.execute(serviceUrl , queryParameters)
         return resp
     
     def get_series(self, collection = None , modality = None , studyInstanceUid = None , outputFormat = "json" ):
-        serviceUrl = self.baseUrl + "/" + self.GET_SERIES
+        serviceUrl = self.baseUrl + "/query/" + self.GET_SERIES
         queryParameters = {"Collection" : collection , "StudyInstanceUID" : studyInstanceUid , "Modality" : modality , "format" : outputFormat }
         resp = self.execute(serviceUrl , queryParameters)
         return resp
     
     def get_patient(self,collection = None , outputFormat = "json" ):
-        serviceUrl = self.baseUrl + "/" + self.GET_PATIENT
+        serviceUrl = self.baseUrl + "/query/" + self.GET_PATIENT
         queryParameters = {"Collection" : collection , "format" : outputFormat }
         resp = self.execute(serviceUrl , queryParameters)
         return resp
     
     def get_image(self , seriesInstanceUid):
-        serviceUrl = self.baseUrl + "/" + self.GET_IMAGE
+        serviceUrl = self.baseUrl + "/query/" + self.GET_IMAGE
         queryParameters = { "SeriesInstanceUID" : seriesInstanceUid }
         resp = self.execute( serviceUrl , queryParameters)
         return resp
