4ae898f7a8c6540603b1904f168e73b536f62468
Ameen Kazerouni <ameen.kazerouni@emory.edu>
2014-10-24 22:18:26 -0400

    Sample code for API consumption

---
+++ tcia-rest-client-python/src/example.py
@@ -0,0 +1,67 @@
+from tciaclient import TCIAClient
+import urllib2, urllib,sys
+####################################  Function to print server response #######
+def printServerResponse(response):
+    if response.getcode() == 200:
+        print "Server Returned:\n"
+        print response.read()
+        print "\n"
+    else:
+        print "Error: " + str(response.getcode())
+
+####################################  Create Clients for Two Different Resources  ####
+tcia_client = TCIAClient(apiKey = "YOUR API KEY HERE",baseUrl="https://services.cancerimagingarchive.net/services/v2",resource = "TCIA")
+tcia_client2 = TCIAClient(apiKey ="YOUR API KEY HERE",baseUrl="https://services.cancerimagingarchive.net/services/v2",resource="SharedList")
+
+# Test content_by_name
+try:
+    response = tcia_client2.contents_by_name(name = "sharedListApiUnitTest")
+    printServerResponse(response);
+except urllib2.HTTPError, err:
+    print "Errror executing program:\nError Code: ", str(err.code), "\nMessage:", err.read()
+
+# Test get_manufacturer_values no query parameters
+try:
+    response = tcia_client.get_manufacturer_values(collection =
+                                                   None,bodyPartExamined =
+                                                   None,modality =
+                                                   None,outputFormat = "json")
+    printServerResponse(response);
+
+except urllib2.HTTPError, err:
+    print "Errror executing program:\nError Code: ", str(err.code), "\nMessage:", err.read()
+
+# Test get_series_size
+try:
+    response = tcia_client.get_series_size(SeriesInstanceUID="1.3.6.1.4.1.14519.5.2.1.7695.4001.306204232344341694648035234440")
+    printServerResponse(response);
+except urllib2.HTTPError, err:
+    print "Errror executing program:\nError Code: ", str(err.code), "\nMessage:", err.read()
+
+# Test get_manufacturer_values with query Parameter
+try:
+    response = tcia_client.get_manufacturer_values(collection = "TCGA-GBM",bodyPartExamined = None,modality = None, outputFormat = "csv")
+    printServerResponse(response);
+
+except urllib2.HTTPError, err:
+    print "Errror executing program:\nError Code: ", str(err.code), "\nMessage:", err.read()
+
+# Test get_image. 
+# NOTE: Image response consumed differently
+try:
+    reponse = tcia_client.get_image(seriesInstanceUid ="1.3.6.1.4.1.14519.5.2.1.7695.4001.306204232344341694648035234440");
+
+    if response.getcode() == 200:
+        print "\n" + str(response.info())
+        bytesRead = response.read()
+        fout = open("images.zip","wb")
+        fout.write(bytesRead)
+        fout.close()
+        print "\nDownloaded file images.zip from the server"
+    else:
+        print "Error : " + str(response.getcode)
+        print "\n" + str(response.info())
+
+except urllib2.HTTPError, err:
+    print "Errror executing program:\nError Code: ", str(err.code), "\nMessage:", err.read()
+
